362afc7059e6ce20f8ef62e630bbe575233fe53b
rootmyth <rootmyth@gmail.com>
2022-02-04 22:36:37 -0600

    Created seed files, imported/invoked them in main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,32 @@
 console.log("Welcome to the main module")
 
+
+// Importing the seeds
+import { createAsparagus } from "./seeds/asparagus.js"
+import { createCorn } from "./seeds/corn.js"
+import { createPotato } from "./seeds/potato.js"
+import { createSoybean } from "./seeds/soybean.js"
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js"
+
+// Creating the seeds
+createAsparagus()
+createCorn()
+createPotato()
+createSoybean()
+createSunflower()
+createWheat()
+
+// vvvvvvv UNCOMMENT BELOW TO VERIFY SEEDS ARE IMPORTING AND RETURNING PROPERLY vvvvvvv //
+
+// const seedGroup = [
+//     createAsparagus(),
+//     createCorn(),
+//     createPotato(),
+//     createSoybean(),
+//     createSunflower(),
+//     createWheat()
+// ]
+// console.log(seedGroup)
+
+// ^^^^^^ UNCOMMENT ABOVE TO VERIFY SEEDS ARE IMPORTING AND RETURNING PROPERLY ^^^^^^^ //
